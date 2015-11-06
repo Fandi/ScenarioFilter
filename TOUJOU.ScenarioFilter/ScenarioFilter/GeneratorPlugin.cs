9bd053e7f0a8ef853740a403aae519053f28fced
Fandi <community.fandi.asywal@outlook.com>
2015-11-06 22:21:58 +0000

    Added initial project files

---
+++ TOUJOU.ScenarioFilter/ScenarioFilter/GeneratorPlugin.cs
@@ -0,0 +1,25 @@
+﻿using BoDi;
+using TechTalk.SpecFlow.Generator.Configuration;
+using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+
+[assembly: TechTalk.SpecFlow.Infrastructure.GeneratorPlugin(typeof(TOUJOU.ScenarioFilter.GeneratorPlugin))]
+
+namespace TOUJOU.ScenarioFilter
+{
+    public class GeneratorPlugin : IGeneratorPlugin
+    {
+        void IGeneratorPlugin.RegisterConfigurationDefaults(SpecFlowProjectConfiguration specFlowConfiguration)
+        {
+        }
+
+        void IGeneratorPlugin.RegisterCustomizations(ObjectContainer container, SpecFlowProjectConfiguration generatorConfiguration)
+        {
+            container.RegisterTypeAs<UnitTestGeneratorProvider, IUnitTestGeneratorProvider>();
+        }
+
+        void IGeneratorPlugin.RegisterDependencies(ObjectContainer container)
+        {
+        }
+    }
+}
---
+++ TOUJOU.ScenarioFilter/UnitTestProject/SpecFlowFeature.feature
@@ -0,0 +1,26 @@
+﻿@filter:A
+Feature: Feature
+
+Scenario: Feature filtered by A
+    Then this scenario should pass
+
+@filter:B
+Scenario: Scenario filtered by B
+    Then this scenario should pass
+
+@filter:C
+@filter:B,A
+Scenario: Scenario filtered by A, B, and C
+    Then this scenario should pass
+
+@filter:A,!B
+Scenario: Scenario filtered by A and !B
+    Then this negative scenario should be inconclusive
+
+@filter:D
+Scenario: Scenario filtered by D
+    Then this negative scenario should be inconclusive
+
+@filter:!E @custom
+Scenario: Scenario filtered by !E and has a custom tag
+    Then this scenario should pass
